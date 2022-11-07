3934f0d9df76ec7db466c4690195d033b0a4b1d1
tanfanhua <tanfanhua@laiye.com>
2022-11-07 10:44:06 +0800

    add

--- src/com/blankj/easy/_0066/Solution.java
+++ src/com/blankj/easy/_0066/Solution.java
@@ -1,6 +1,9 @@
 package com.blankj.easy._066;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <pre>
@@ -33,5 +36,57 @@ public class Solution {
         Solution solution = new Solution();
         int[] digits = solution.plusOne(new int[]{9, 9, 9});
         System.out.println(Arrays.toString(digits));
+        digits = solution.myPlusOne(new int[]{9, 9, 9});
+        System.out.println(Arrays.toString(digits));
+        digits = solution.myPlusOne2(new int[]{9, 9, 9});
+        System.out.println(Arrays.toString(digits));
+        digits = solution.myPlusOne2(new int[]{8, 9, 9});
+        System.out.println(Arrays.toString(digits));
+        digits = solution.myPlusOne2(new int[]{8, 9, 8});
+        System.out.println(Arrays.toString(digits));
+
+    }
+
+    public int[] myPlusOne(int[] digits) {
+        if (digits == null || digits.length == 0) {
+            return null;
+        }
+        int[] res = new int[digits.length];
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            res[i] = (digits[i] + carry) % 10;
+            carry = (digits[i] + carry) / 10;
+        }
+        if (carry > 0) {
+            int[] tmp = new int[digits.length + 1];
+            tmp[0] = carry;
+            for (int i = 0; i < res.length; i++) {
+                tmp[i + 1] = res[i];
+            }
+            res = tmp;
+        }
+
+        return res;
+    }
+
+    public int[] myPlusOne2(int[] digits) {
+        if (digits == null || digits.length == 0) {
+            return null;
+        }
+        int i = digits.length - 1;
+        for (; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                break;
+            }
+        }
+        if (i < 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        } else {
+            digits[i] ++;
+        }
+        return digits;
     }
 }
