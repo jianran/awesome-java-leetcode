e153157800a0fd86ba6e8dc045c7f6b45b6c58ee
tanfanhua <tanfanhua@laiye.com>
2022-11-08 10:01:51 +0800

    left

--- src/com/blankj/easy/_0067/Solution.java
+++ src/com/blankj/easy/_0067/Solution.java
@@ -49,5 +49,51 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.addBinary("11", "1"));
+        System.out.println(solution.myAddBinary("11", "1"));
+        System.out.println(solution.addBinary("11", "111"));
+        System.out.println(solution.myAddBinary("11", "111"));
+        System.out.println(solution.addBinary("101", "1110"));
+        System.out.println(solution.myAddBinary("101", "1110"));
+
+    }
+
+    public String myAddBinary(String a, String b) {
+        if (a == null || a.length() == 0) {
+            return b;
+        }
+        if (b == null || b.length() == 0) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        int aLength = a.length();
+        int bLength = b.length();
+        int minLength = Math.min(aLength, bLength);
+        int carry = 0;
+        for (int i = 0; i < minLength; i++) {
+            char aChar = a.charAt(aLength - 1 - i);
+            char bChar = b.charAt(bLength - 1 - i);
+            carry += aChar - '0' + bChar - '0';
+            sb.insert(0, (char)('0' + carry % 2));
+            carry >>= 1;
+        }
+        String leftStr = a;
+        if (aLength < bLength) {
+            leftStr = b;
+        }
+        carry = addLeft(sb, leftStr, minLength, carry);
+        if (carry != 0) {
+            sb.insert(0, '1');
+        }
+        return sb.toString();
+    }
+
+    private int addLeft(StringBuilder sb, String leftStr, int minLength, int carry) {
+        for (int i = minLength; i < leftStr.length(); i++) {
+            char aChar = leftStr.charAt(leftStr.length() - 1 - i);
+            carry += aChar - '0';
+            sb.insert(0, (char)('0' + carry % 2));
+            carry >>= 1;
+        }
+        return carry;
     }
 }
